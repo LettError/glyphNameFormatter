dc035287cc5af31ea0d7f9370a3a9e10325d140a
Roberto Arista <arista.rob@gmail.com>
2024-02-12 15:10:52 +0100

    ethiopic extended and supplement WIP

---
+++ Lib/glyphNameFormatter/rangeProcessors/ethiopic_extended_b.py
@@ -0,0 +1,14 @@
+def process(self):
+    self.edit("ETHIOPIC")
+    self.edit("SYLLABLE")
+    self.edit("TONAL")
+    self.editToFinal("MARK", "mark")
+    self.lower()
+    self.compress()
+    self.scriptPrefix()
+
+
+if __name__ == "__main__":
+    from glyphNameFormatter.exporters import printRange
+
+    printRange("Ethiopic Extended-B")
